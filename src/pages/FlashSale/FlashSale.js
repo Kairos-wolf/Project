a826a7d4950ffbbf825de4dc4b3ba9b324cc3ae2
Kairos-wolf <vietlinh16092004@gmail.com>
2025-03-12 21:58:38 +0700

    PJ

---
+++ src/pages/FlashSale/FlashSale.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FlashSale = () => {
+  return (
+    <div>
+      <h2>🔥 Flash Sale đang diễn ra! 🔥</h2>
+      <p>Giảm giá sốc trong thời gian có hạn, hãy mua ngay!</p>
+    </div>
+  );
+};
+
+export default FlashSale;
